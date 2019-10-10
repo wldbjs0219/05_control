abba3d0f728bacb63a691b9c6be16ac187a0e0c7
wldbjs0219 <55428633+wldbjs0219@users.noreply.github.com>
2019-10-10 16:39:15 +0900

    05-5

--- main.c
+++ main.c
@@ -3,21 +3,11 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) 
+int main (int argc, char *argv[])
 {
-<<<<<<< HEAD
 	int i;
 	for (i=0; i<10; i++)
-	 printf("Hello World!\n");
-=======
-	int i=0,meter;
-	
-	while (i<3)
-	{
-		meter=i*1609;
-		printf("%d 마일은 %d 미터입니다. \n", i,meter);
-		i++;
-	}
->>>>>>> 9bafbac882569549e8a42e00559973bfee2a59af
-	return 0;
+	 printf("Hello World! \n");
+	 
+	 return 0;
 }
