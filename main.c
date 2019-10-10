fcecd90bd5ef5ed48af496337465582fad013e70
wldbjs0219 <55428633+wldbjs0219@users.noreply.github.com>
2019-10-10 17:06:44 +0900

    05-7

--- main.c
+++ main.c
@@ -5,21 +5,29 @@
 
 int main (int argc, char *argv[]) {
 	
-	char c;
-	int num=0;
+	int answer= 59;
+	int input;
+	int cnt=0;
 	
-	printf("input a string : ");
-	
-	while ((c=getchar()) != '\n' )
+	do
 	{
-		   if (c<= '9' && c>= '0')
-		   {
-		   	     num++;
-		   	    
-			}
+		printf("Guess a number : ");
+		scanf("%d", &input);
+		
+		if (input < answer)
+		{
+			printf("low!\n");
+			
+		}
+		else if (input > answer)
+		{
+			printf("high!\n");
+		}
+		cnt++;
 	}
-	
-	printf("the number of degits is : %i\n", num);
+	while (input != answer);
+	printf ("congratulation. trial : %i\n", cnt);
+
     return 0;
     
 }
